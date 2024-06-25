d01e41c99a8ce7a1396f8b55ce9b6de0b77b1d49
Nguyễn Quân <mquan142000@gmail.com>
2024-06-25 16:55:29 +0700

    ex-sort

---
+++ module-2/session-14/bubble-sort-by-step/src/BubbleSortByStep.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class BubbleSortByStep {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int size = scanner.nextInt();
+        int[] list = new int[size];
+        System.out.println("Enter " + list.length + "values: ");
+        for (int i = 0; i < list.length; i++) {
+            list[i] = scanner.nextInt();
+        }
+
+        System.out.println("Your input list: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        bubbleSortByStep(list);
+    }
+
+    public static void bubbleSortByStep(int[] list) {
+        boolean needNextPass = true;
+        for (int i = 1; i < list.length && needNextPass; i++) {
+            needNextPass = false;
+            for (int j = 0; j < list.length - i; j++) {
+                if (list[j] > list[j + 1]) {
+                    System.out.println("Swap " + list[j] + " and " + list[j + 1]);
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                    needNextPass = true;
+                }
+            }
+
+            if (needNextPass == false) {
+                System.out.println("Array may be sorted and next pass not needed");
+                break;
+            }
+            System.out.print("List after the  " + i + "' sort: ");
+            for (int k = 0; k < list.length; k++) {
+                System.out.print(list[k] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ module-2/session-14/insertion-sort/src/InsertionSort.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter list size: ");
+        int size = scanner.nextInt();
+        int[] list = new int[size];
+        System.out.println("Enter " + list.length + " values:");
+        for (int i = 0; i < list.length; i++) {
+            list[i] = scanner.nextInt();
+        }
+        System.out.print("Your input list: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        System.out.println();
+        insertionSort(list);
+    }
+
+    public static void insertionSort(int[] list) {
+        for (int i = 1; i < list.length; i++) {
+            int currentElement = list[i];
+            int k;
+            for (k = i - 1; k >= 0 && list[k] > currentElement; k--) {
+                list[k + 1] = list[k];
+            }
+            list[k + 1] = currentElement;
+
+            System.out.print("Mảng sau khi sắp xếp " + i + ": ");
+            for (int j = 0; j < list.length; j++) {
+                System.out.print(list[j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+}
